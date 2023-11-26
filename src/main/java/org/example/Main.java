6cfa5dcd5f9285d246b979da48c6e93c62177fb4
sundance1807 <aalyiev7@gmail.com>
2023-11-26 20:26:15 +0600

    Console Menu

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -19,19 +19,19 @@ public class Main {
         printer.printMenu(films);
 
 
-//        Scanner sc = new Scanner(System.in);
-//        String[] types = {"По рейтингу", "По количеству оценок", "По жанру", "Выйти"};
-//        while (true) {
-//            System.out.println("\nВыберите тип сортировки:");
-//            for (int i = 0; i < types.length; i++) {
-//                System.out.println((i + ": " + types[i]));
-//            }
-//            int choice = sc.nextInt();
-//            if (choice == 3) {
-//                break;
-//            }
-//            fas.getFilmsByFilterType(choice);
-//            System.out.println();
-//        }
+        Scanner sc = new Scanner(System.in);
+        String[] types = {"По рейтингу", "По количеству оценок", "По жанру", "Выйти"};
+        while (true) {
+            System.out.println("\nВыберите тип сортировки:");
+            for (int i = 0; i < types.length; i++) {
+                System.out.println((i + ": " + types[i]));
+            }
+            int choice = sc.nextInt();
+            if (choice == 3) {
+                break;
+            }
+            fas.getFilmsByFilterType(choice);
+            System.out.println();
+        }
     }
 }
